adaca7cc2c7f807660b6494fd3dd7c883cea0fa1
Ollie Ford <me@ojford.com>
2016-12-19 17:43:55 +0000

    Be more verbose during init script

--- init.sh
+++ init.sh
@@ -9,20 +9,27 @@ repo="/var/git/$repo_name"
 served="/var/www/$repo_name"
 
 if ! hash caddy 2>/dev/null; then
+    echo "Installing Caddy with: $caddy_features..."
     build_params="os=linux&arch=amd64&features=${caddy_features// /%2C}"
     wget "https://caddyserver.com/download/build?$build_params" -O caddy.gz
     tar -xvf caddy.gz --directory=/tmp
     mv /tmp/caddy /usr/local/bin
+
+    echo "Setting Caddy permissions..."
     chmod 755 /usr/local/bin/caddy
+
+    echo "Setting Caddy to run on startup..."
     mv /tmp/init/linux-systemd/caddy.service /etc/systemd/system
     chmod 644 /etc/systemd/system/caddy.service
+    systemctl enable caddy
 
-    # Non-root access to ports <1024
+    echo "Giving Caddy  access to ports <1024..."
     setcap 'cap_net_bind_service=+ep' /usr/local/bin/caddy
 
+    echo "Setting Caddy to clone & run $repo..."
     mkdir -p /etc/caddy
     # Dummy Caddyfile that serves only to clone the repo with the real one
-    cat << EOF > /etc/caddy/Caddyfile
+    cat << END_CF > /etc/caddy/Caddyfile
 ojford.com
 tls off
 git {
@@ -32,17 +39,18 @@ git {
     then git --git-dir=$repo/.git checkout-index -a -f --prefix=$served/
     then ln -sf $served/Caddyfile /etc/caddy/Caddyfile 
 }
-EOF
-    chown -R root:www-data /etc/caddy
-    chmod 444 /etc/caddy/Caddyfile
+END_CF
     mkdir -p "$repo" "$served"
 
+    echo "Setting permissions on Caddy data..."
+    chown -R root:www-data /etc/caddy
+    chmod 444 /etc/caddy/Caddyfile
+
+    echo "Setting permissions on Caddy SSL data..."
     mkdir -p /etc/ssl/caddy
     chown -R www-data:root /etc/ssl/caddy
     chmod 0770 /etc/ssl/caddy
 
-    # Squashes Caddy warning
+    echo "Changing misc. Caddy-recommended settings..."
     ulimit -n 8192
 fi
-
-systemctl enable caddy
